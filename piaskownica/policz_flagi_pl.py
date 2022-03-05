66497e59b448d93d3e5bebccbf4b69bd500349a1
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-05 22:34:22 +0100

    ok

---
+++ piaskownica/policz_flagi_pl.py
@@ -0,0 +1,12 @@
+from lista_flag.py import stworz_liste_flag
+
+def policz_domeny_pl(linki):
+    wynik = 0
+
+
+    return wynik
+
+lista_flag = stworz_liste_flag()
+linki = []
+wynik = policz_domeny_pl(lista_flag)
+print(wynik)
